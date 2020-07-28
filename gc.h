33564980345b1a2204eb97020ebdc17e5f70d18e
Raymond Jennings <shentino@gmail.com>
2020-07-27 22:37:22 -0700

    Add width and height as protected properties in gc, to be set by the derived class's prepare method

--- gc.h
+++ gc.h
@@ -1,11 +1,16 @@
 #ifndef GC_H
 #define GC_H
 
+#include <cstdint>
+
 class gc
 {
 	gc();
 	gc(const gc &);
 	gc &operator=(const gc &);
+
+protected:
+	uint16_t width, height;
 
 public:
 	virtual ~gc();
