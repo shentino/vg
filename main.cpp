754623c331ff6fa3fe194b311df3ea1f6ec0fb56
Raymond Jennings <shentino@gmail.com>
2020-07-27 22:29:36 -0700

    Start SDL subsystem

--- main.cpp
+++ main.cpp
@@ -8,4 +8,14 @@ int main(int argc, char *argv[], char *envp[])
 	using namespace std;
 
 	cout << "Hello world" << endl;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		throw runtime_error(SDL_GetError());
+	}
+
+	cout << "SDL Initialized" << endl;
+
+	SDL_Delay(1000);
+
+	SDL_Quit();
 }
