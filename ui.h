cc4ae590b318b6ede01392ae301d504bd9153bfe
Raymond Jennings <shentino@gmail.com>
2020-07-30 07:54:19 -0700

    Fill out ui class

--- ui.h
+++ ui.h
@@ -1,10 +1,24 @@
+/*
+the UI class is responsible for keeping track of all data related to the UI
+
+this includes cursor position and zoom level and anything that the board and gc shouldn't have to worry about
+*/
 #ifndef UI_H
 #define UI_H
+
+class gc;
+class board;
 
 class ui
 {
 	ui(const ui &);
 	ui &operator=(const ui &);
+
+public:
+	int cx, cy; // cursor position
+	int zoom; // zoom factor
+
+	void draw(gc *g, const board *b);
 };
 
 #endif
