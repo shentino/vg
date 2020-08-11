9c744fae3f4808413f8625662e3cb90ae2388934
Raymond Jennings <shentino@gmail.com>
2020-08-11 06:46:06 -0700

    For now, sdlglgc can use inherited methods from sdlgc

--- src/sdlglgc.h
+++ src/sdlglgc.h
@@ -11,12 +11,6 @@ class sdlglgc: public sdlgc
 public:
 	sdlglgc(const char *title);
 	virtual ~sdlglgc();
-
-	virtual void prepare();
-	virtual void render();
-
-	virtual void clear(int c);
-	virtual void box(int x, int y, int w, int h, int c, int lw);
 };
 
 #endif
