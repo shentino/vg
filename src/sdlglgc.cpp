c55c3e366f885bee8da69606bf31ee8e123f8f82
Raymond Jennings <shentino@gmail.com>
2020-08-11 06:40:01 -0700

    Remove these stubs, they're badly formatted anyway with virtual keywords

--- src/sdlglgc.cpp
+++ src/sdlglgc.cpp
@@ -32,19 +32,3 @@ sdlglgc::sdlglgc(const char *title)
 
 	SDL_MaximizeWindow(win);
 }
-
-virtual void prepare()
-{
-}
-
-virtual void render()
-{
-}
-
-virtual void clear(int c)
-{
-}
-
-virtual void box(int x, int y, int w, int h, int c, int lw)
-{
-}
