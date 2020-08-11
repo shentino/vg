ec6d4ca9771bad29b9b7159483cf3bdadaa86c64
Raymond Jennings <shentino@gmail.com>
2020-08-11 03:51:52 -0700

    Implement constructor and destructor in sdlglgc, copied from sdlgc, considering derivation

--- src/sdlglgc.cpp
+++ src/sdlglgc.cpp
@@ -1,11 +1,41 @@
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+#include <SDL2/SDL.h>
+
 #include "sdlglgc.h"
 
-sdlglgc(const char *title)
+#include <stdexcept>
+
+#include <SDL2/SDL.h>
+
+#include "sdlgc.h"
+
+using namespace std;
+
+sdlglgc::sdlglgc(const char *title)
 {
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	win = SDL_CreateWindow(title
+		, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED
+		, 640, 480, SDL_WINDOW_RESIZABLE | SDL_OPENGL
+	);
+
+	if (!win) {
+		throw runtime_error(SDL_GetError());
+	}
+
+	SDL_MaximizeWindow(win);
 }
 
-virtual ~sdlglgc()
+sdlglgc::~sdlglgc()
 {
+	SDL_DestroyWindow(win);
 }
 
 virtual void prepare()
