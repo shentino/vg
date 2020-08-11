7288bd451c51e316bbd9f1fcb15d5a00e162c282
Raymond Jennings <shentino@gmail.com>
2020-08-11 03:41:45 -0700

    Add stub for sdlglgc.cpp

---
+++ src/sdlglgc.cpp
@@ -0,0 +1,25 @@
+#include "sdlglgc.h"
+
+sdlglgc(const char *title)
+{
+}
+
+virtual ~sdlglgc()
+{
+}
+
+virtual void prepare()
+{
+}
+
+virtual void render()
+{
+}
+
+virtual void clear(int c)
+{
+}
+
+virtual void box(int x, int y, int w, int h, int c, int lw)
+{
+}
