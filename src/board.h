746641de6b2258ee467a73abeb7116839c803964
Raymond Jennings <shentino@gmail.com>
2020-09-27 21:54:41 -0700

    Move offset function to ui class, it really is the ui's prerogative to decide where the board is drawn

--- src/board.h
+++ src/board.h
@@ -29,32 +29,6 @@ public:
 
 		return cells[sx * y + x];
 	}
-
-	// returns: where to draw the home end of the board
-	static int offset(int c, int w, int b, int f)
-	{
-		// c - collar size
-		// w - window size
-		// b - board size
-		// f - focus point
-		int o;
-
-		if (w >= (b + c * 2)) {
-			// room for the whole thing
-			return (w - b) / 2;
-		}
-
-		if (f + c < w / 2) {
-			// focus is close enough to left edge
-			return c;
-		} else if (c + b - f < w / 2) {
-			// focus is close enough to right edge
-			return w - b - c;
-		} else {
-			// squeeze time
-			return (w / 2) - f;
-		}
-	}
 };
 
 #endif
--- src/ui.h
+++ src/ui.h
@@ -30,6 +30,31 @@ public:
 
 	ui();
 
+	static int offset(int c, int w, int b, int f)
+	{
+		// c - collar size
+		// w - window size
+		// b - board size
+		// f - focus point
+		int o;
+
+		if (w >= (b + c * 2)) {
+			// room for the whole thing
+			return (w - b) / 2;
+		}
+
+		if (f + c < w / 2) {
+			// focus is close enough to left edge
+			return c;
+		} else if (c + b - f < w / 2) {
+			// focus is close enough to right edge
+			return w - b - c;
+		} else {
+			// squeeze time
+			return (w / 2) - f;
+		}
+	}
+
 	void draw(gc *g, const board *b);
 	void tick(); // once per frame
 	void click(int x, int y); // mouse click
