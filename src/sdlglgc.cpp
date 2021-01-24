de93eec6df028efbbc79f278a47beb12c0b6fe02
Raymond Jennings <shentino@gmail.com>
2021-01-24 04:18:48 -0800

    Finally implement the GL version of the GC

--- src/sdlglgc.cpp
+++ src/sdlglgc.cpp
@@ -14,7 +14,7 @@ sdlglgc::sdlglgc(const char *title)
 	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 0);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 1);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
 	win = SDL_CreateWindow(title
@@ -25,8 +25,49 @@ sdlglgc::sdlglgc(const char *title)
 	if (!win) {
 		throw runtime_error(SDL_GetError());
 	}
+
+	glc = SDL_GL_CreateContext(win);
 }
 
 sdlglgc::~sdlglgc()
 {
+	SDL_GL_DeleteContext(glc);
+}
+
+void sdlglgc::prepare()
+{
+	sdlgc::prepare();
+
+	SDL_GL_MakeCurrent(win, glc);
+
+	glLoadIdentity();
+	glViewport(0, 0, width, height);
+	glOrtho(0, width, height, 0, 0, 1);
+}
+
+void sdlglgc::render()
+{
+	SDL_GL_SwapWindow(win);
+}
+
+void sdlglgc::clear(int c)
+{
+	int r = (c >> 16) & 0xff;
+	int g = (c >> 8) & 0xff;
+	int b = c & 0xff;
+
+	glClearColor(r / 255.0f, g / 255.0f, b / 255.0f, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void sdlglgc::box(int x, int y, int w, int h, int c)
+{
+	glColor3ub((c >> 16) & 0xff, (c >> 8) & 0xff, c & 0xff);
+
+	glBegin(GL_TRIANGLE_FAN);
+		glVertex2i(x, y);
+		glVertex2i(x + w, y);
+		glVertex2i(x + w, y + h);
+		glVertex2i(x, y + h);
+	glEnd();
 }
--- src/sdlglgc.h
+++ src/sdlglgc.h
@@ -8,9 +8,17 @@
 
 class sdlglgc: public sdlgc
 {
+	SDL_GLContext glc;
+
 public:
 	sdlglgc(const char *title);
 	virtual ~sdlglgc();
+
+	virtual void prepare();
+	virtual void render();
+
+	virtual void clear(int c);
+	virtual void box(int x, int y, int w, int h, int c);
 };
 
 #endif
