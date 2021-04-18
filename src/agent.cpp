a38045424050ef8bdbd2907aec0fce45060e159d
Raymond Jennings <shentino@gmail.com>
2021-04-18 12:50:24 -0700

    Add stub for agent::draw

--- src/agent.cpp
+++ src/agent.cpp
@@ -1,5 +1,6 @@
 #include "agent.h"
 #include "board.h"
+#include "gc.h"
 
 // check the board, return which way we want to go
 void seek(board *b, float &dx, float &dy)
@@ -9,4 +10,16 @@ void seek(board *b, float &dx, float &dy)
 // burn health
 void tick()
 {
+}
+
+void agent::draw(gc *gc, int cellsize, int ox, int oy, int cc) const
+{
+	int x, y;
+
+	gc->box(
+		x * cellsize + ox,
+		y * cellsize + oy,
+		cellsize, cellsize,
+		0x000000
+	);
 }
