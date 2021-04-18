bc81a8c8d2ddda15d20e687dea3ba872fc2a402c
Raymond Jennings <shentino@gmail.com>
2021-04-18 15:34:53 -0700

    Fix bug in agent drawing where we clobbered the class variable, also clamp the color value

--- src/agent.cpp
+++ src/agent.cpp
@@ -19,14 +19,31 @@ void agent::tick()
 	health -= 1.0 / 60.0;
 }
 
-void agent::draw(gc *gc, int cellsize, int ox, int oy, int cc) const
+void agent::draw(gc *g, int cellsize, int ox, int oy, int cc) const
 {
-	int x, y;
+	float lhealth;
+	int c;
 
-	gc->box(
+	lhealth = health;
+
+	if (lhealth < -5.0) {
+		lhealth = -5.0;
+	} else if (lhealth > 5.0) {
+		lhealth = 5.0;
+	}
+
+	if (lhealth > 0.0) {
+		// white, decaying to black
+		c = 0x010101 * (int)(lhealth * 255.0 / 5.0);
+	} else {
+		// black, decaying to red
+		c = 0x010000 * (int)(-lhealth * 255.0 / 5.0);
+	}
+
+	g->box(
 		x * cellsize + ox,
 		y * cellsize + oy,
 		cellsize, cellsize,
-		0x000000
+		c
 	);
 }
