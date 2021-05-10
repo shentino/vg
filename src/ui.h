809fddbf78631b7ea2cc1c1a5e09eece8e951976
Raymond Jennings <shentino@gmail.com>
2021-05-10 10:26:04 -0700

    Add copyright notices to Makefile, *.cpp, and *.h

--- src/ui.h
+++ src/ui.h
@@ -1,4 +1,21 @@
 /*
+ * Video Game Prototype
+ * Copyright (C) 2021  Raymond Jennings
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+/*
 the UI class is responsible for keeping track of all data related to the UI
 
 this includes cursor position and zoom level and anything that the board and gc shouldn't have to worry about
